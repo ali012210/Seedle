3a4e401ea04f8472fdae89bff76de502a76d6347
ali012210 <ali-eideh@live.com.au>
2024-02-22 10:44:57 +1100

    Database connection setup

--- server/index.js
+++ server/index.js
@@ -5,8 +5,26 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+
+const db = mongoose.connection;
+db.on('connected', () => console.log('MongoDB connected'));
+db.on('error', (err) => console.log('MongoDB connection error: ', err));
+db.on('disconnected', () => console.log('MongoDB disconnected'));
+
 const express = require('express');
 const app = express();
+
+// Import routes
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
+
+// Middleware
+app.use(express.json()); // for parsing application/json
+
+// Use routes
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
