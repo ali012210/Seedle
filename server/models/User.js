7eddb1e05bcfa45a051d8d7d97ce9c24c0abbdd7
ali012210 <ali-eideh@live.com.au>
2024-02-28 10:43:29 +1100

    Mongoose model hashing instantiated

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,6 +17,23 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+// Pre-save hook to hash the password before saving the user to the database
+userSchema.pre('save', function (next) {
+    // Check if the password has been modified (or new) to avoid rehashing the password when updating user details
+    if (!this.isModified('password')) return next();
+
+    // Salt and hash the password using bcrypt
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+            // Replace the plain text password with the hash and call next
+            this.password = hash;
+            next();
+        });
+    });
 });
 
 const User = mongoose.model('User', userSchema);
