7ed956f1974ef7d5bb2b823c51319371dba65699
ali012210 <ali-eideh@live.com.au>
2024-03-01 11:42:41 +1100

    Cutting Mongo URI and placing it in .env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const MONGO_URI = "mongodb+srv://ali012210:Eid3433417@seedle-dev.2r8bul2.mongodb.net/?retryWrites=true&w=majority&appName=Seedle-Dev";
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
