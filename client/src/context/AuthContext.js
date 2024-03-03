e427047fb351df28e26bad41542372c054f3f524
ali012210 <ali-eideh@live.com.au>
2024-03-03 20:52:15 +1100

    Frontend Dev begins with file structure initialisation and the authentication context setup

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer} from 'react';
+
+export const AuthContext = createContext();
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                isAuthenticated: true,
+                user: action.payload,
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <AuthContext.Provider value={{ state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
