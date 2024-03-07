034073f007c4884466f27d3c86bff5fe7ee0623b
ali012210 <ali-eideh@live.com.au>
2024-03-07 19:20:13 +1100

    AppRoutes base configuration

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import PostsList from './components/PostsList';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserProfile from './pages/UserProfile';
+import PostCreationPage from './pages/PostCreationPage';
+import UserProfileEdit from './pages/UserProfileEdit';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import PostViewPage from './pages/PostViewPage';
+import UserOverview from './pages/UserOverview';
+import PrivateRoute from './components/PrivateRoute'; // A component for handling private routes
 
-const AppRoutes = () => {
+function AppRoutes() {
     return (
-        <Switch>
-            <Route path="/" exact component={PostsList} />
-            {/* Add more routes here */}
-        </Switch>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <PrivateRoute path="/user/:userID" component={UserOverview} />
+                <PrivateRoute path="/post/new" component={PostCreationPage} />
+                <PrivateRoute path="/profile" exact component={UserProfile} />
+                <PrivateRoute path="/profile/edit" component={UserProfileEdit} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/signup" component={SignUpPage} />
+                <Route path="/:postID/comments" component={PostViewPage} />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
     );
 };
 
